2d394e8d2e29aa546f496d211a97f527fb6b85d8
JJ Lu-Hilson <jjluhilson@gmail.com>
2025-02-25 02:36:34 +0800

    dockerised dog - can just start container using make run-debug and call ros2 run dog_interface dog_interface to control

---
+++ unitree_go2/dog_interface/src/dog-stand.cpp
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <cmath>
+
+#include "rclcpp/rclcpp.hpp"
+#include "unitree_go/msg/sport_mode_state.hpp"
+
+#include "unitree_api/msg/request.hpp"
+#include "common/ros2_sport_client.h"
+
+#include "geometry_msgs/msg/twist.hpp"
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv); // Initialize rclcpp
+
+    auto node = rclcpp::Node::make_shared("req_sender");
+
+    rclcpp::Publisher<unitree_api::msg::Request>::SharedPtr req_puber;
+    unitree_api::msg::Request req; // Unitree Go2 ROS2 request message
+    SportClient sport_req;
+
+    req_puber = node->create_publisher<unitree_api::msg::Request>("/api/sport/request", 10);
+
+    sleep(2); // Wait for initialisation or this doesn't work
+    
+    sport_req.BalanceStand(req);
+    req_puber->publish(req);
+    
+    sleep(2);
+
+    rclcpp::shutdown();
+    return 0;
+}
