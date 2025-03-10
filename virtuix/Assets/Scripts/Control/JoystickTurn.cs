ee37b056dabc635662c179bff44b91ef349fc2fa
advikchitre <advik.chitre42@gmail.com>
2025-03-10 00:14:23 +0000

    Manual turn

---
+++ virtuix/Assets/Scripts/Control/JoystickTurn.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class JoystickSphere : MonoBehaviour
+{
+    // Assign in inspector
+    public SteamVR_Input_Sources handType; 
+    public SteamVR_Action_Boolean trigger;
+    public Transform sphere;
+    public float rotationSpeed = 45f; // Degrees per second
+
+    void Start()
+    {
+        if handType == SteamVR_Input_Sources.RightHand{
+            rotationSpeed = -rotationSpeed;
+        }
+        else if handType != SteamVR_Input_Sources.LeftHand{
+            Debug.LogError("Invalid hand type");
+        }
+    }
+    
+    void Update()
+    {
+        // Trigger pressed
+        if (trigger.GetState(handType))
+        {
+            // Rotate the sphere around its Y axis at constant speed
+            sphere.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
