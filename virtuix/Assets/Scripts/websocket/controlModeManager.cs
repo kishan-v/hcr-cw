bfa61948d77e041464ccd5c02abef3cf40294b8a
dhruvds12 <dhruv.devgan@yahoo.com>
2025-03-18 15:18:03 +0000

    changed toggle keys

--- virtuix/Assets/Scripts/websocket/controlModeManager.cs
+++ virtuix/Assets/Scripts/websocket/controlModeManager.cs
@@ -3,28 +3,38 @@ using UnityEngine;
 public enum ControlMode
 {
     Virtuix,
-    Joystick
+    Joystick,
+
+    None
 }
 
 public class ControlModeManager : MonoBehaviour
 {
-    public static ControlMode activeMode = ControlMode.Virtuix; // Default mode
+    [SerializeField]
+    public static ControlMode activeMode = ControlMode.None; // Default mode
 
     void Update()
     {
+        buttonPressed =
         // Toggle control mode when T is pressed.
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.X))
         {
-            if (activeMode == ControlMode.Virtuix)
-            {
-                activeMode = ControlMode.Joystick;
-                Debug.Log("Switched to Joystick mode");
-            }
-            else
-            {
-                activeMode = ControlMode.Virtuix;
-                Debug.Log("Switched to Virtuix mode");
-            }
+            activeMode = ControlMode.None;
+            Debug.Log("Switched to NO TRANSMIT")
+
         }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            activeMode = ControlMode.Virtuix;
+            Debug.Log("Switched to Virtuix mode");
+
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            activeMode = ControlMode.Joystick;
+            Debug.Log("Switched to Joystick mode");
+
+        }
+        
     }
 }
